1d33be54a6cd5ff0f4529feddc8de6545c5fd8ef
Wang_yuzhe <woshiregener@gmail.com>
2022-09-06 23:21:45 +0200

    Myfile

--- Wang/main.py
+++ Wang/main.py
@@ -1,5 +1,5 @@
 import torch
-from torch import nn, optim
+from torch import nn, optim, device
 import torch.nn.functional as F
 
 
@@ -9,8 +9,8 @@ class Scheduler(nn.Module):
         self.fc1 = nn.Linear(12, 256)
         self.fc2 = nn.Linear(256, 1024)
         self.fc3 = nn.Linear(1024, 4096)
-        # use dropout and p = 0.2
-        self.dropout = nn.Dropout(0.2)
+        # use dropout to avoid overfitting and p = 0.1
+        self.dropout = nn.Dropout(0.1)
 
     def forward(self, x):
         x = self.dropout(F.relu(self.fc1(x)))
@@ -20,14 +20,13 @@ class Scheduler(nn.Module):
         return x
 
 def train():
+    global running_loss
     model = Scheduler()
     # use cross entropy loss function
     loss = nn.CrossEntropyLoss()
     # use Adam optimizer and learning rate is 0.002
     optimizer = optim.Adam(model.parameters(), lr=0.002)
     epochs = 12
-    # plot and compare
-    train_losses, train_losses = [], []
 
     print('Start')
     for epochs in range(epochs):
@@ -47,11 +46,14 @@ def train():
             # close dropout when testing
             model.eval()
             for data, ORF in testloader:
-                log_ps = model(data)
-                test_loss += loss(log_ps, ORF)
-                ps = torch.exp(log_ps)
+                data, target = data.to(device), ORF.to(device)
+                test_loss += nn.CrossEntropyLoss(data, ORF, reduction='sum').item()  # sum up batch loss
+                pred = ORF
+                accuracy += pred.eq(target.view_as(pred)).sum().item()
+            model.train()
+            # storing the loss of training/testing in lists and plot
 
-
-    model.train()
-    train_losses.append(running_loss / len(trainloader))
-    test_losses.append(test_loss / len(testloader)
+    print("times of learning: {}/{}.. ".format(epochs + 1, epochs),
+          "loss of training: {:.3f}.. ".format(running_loss / len(trainloader)),
+          "loss of testing: {:.3f}.. ".format(test_loss / len(testloader)),
+          "accuracy: {:.3f}".format(accuracy / len(testloader)))
